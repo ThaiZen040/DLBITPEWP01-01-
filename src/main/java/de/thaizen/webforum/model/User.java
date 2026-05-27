5d1d96844ff21bb4f78150339cb20c1fc405d7f8
ThaiZen040 <154074568+ThaiZen040@users.noreply.github.com>
2026-05-27 18:15:48 +0200

    Annotation hinzugefügt Role.java created

--- src/main/java/de/thaizen/webforum/model/User.java
+++ src/main/java/de/thaizen/webforum/model/User.java
@@ -1,14 +1,29 @@
 package de.thaizen.webforum.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String username;
     private String password;
     private String email;
     private String role;
     private String token;
 
+    // Leerer Konstruktor für Spring/JPA
     public User() {
+    }
 
+    // Konstruktor mit Parametern
+    public User(String username, String password, String email, String role, String token) {
         this.username = username;
         this.password = password;
         this.email = email;
@@ -16,6 +31,10 @@ public class User {
         this.token = token;
     }
 
+    // Getter
+    public Long getId() {
+        return id;
+    }
 
     public String getUsername() {
         return username;
@@ -24,30 +43,37 @@ public class User {
     public String getPassword() {
         return password;
     }
+
     public String getEmail() {
         return email;
     }
+
     public String getRole() {
         return role;
     }
+
     public String getToken() {
         return token;
     }
-    public void setToken(String token) {
-        this.token = token;
-    }
+
+    // Setter
     public void setUsername(String username) {
         this.username = username;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public void setRole(String role) {
         this.role = role;
     }
 
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
-
