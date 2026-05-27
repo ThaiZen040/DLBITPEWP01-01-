ef002ebb8778121c174a521532b82b2d7340635d
ThaiZen040 <154074568+ThaiZen040@users.noreply.github.com>
2026-05-27 16:23:52 +0200

    Projektstruktur

---
+++ src/test/java/de/thaizen/webforum/WebforumApplicationTests.java
@@ -0,0 +1,13 @@
+package de.thaizen.webforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebforumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
