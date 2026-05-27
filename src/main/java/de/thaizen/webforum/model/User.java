e2b5a4ff3c8c6921249bd002e1b836ce7e1b103b
ThaiZen040 <154074568+ThaiZen040@users.noreply.github.com>
2026-05-27 17:41:44 +0200

    getter und setter festgelegt. public konstruktor erstellt

--- src/main/java/de/thaizen/webforum/model/User.java
+++ src/main/java/de/thaizen/webforum/model/User.java
@@ -7,9 +7,47 @@ public class User {
     private String role;
     private String token;
 
+    public User() {
+
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+        this.token = token;
+    }
+
 
     public String getUsername() {
         return username;
     }
+
+    public String getPassword() {
+        return password;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getRole() {
+        return role;
+    }
+    public String getToken() {
+        return token;
+    }
+    public void setToken(String token) {
+        this.token = token;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+
 }
 
