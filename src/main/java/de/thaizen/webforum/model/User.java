25c3deaa8622e48933061747248f43bc3a6fedb1
ThaiZen040 <154074568+ThaiZen040@users.noreply.github.com>
2026-05-27 17:29:43 +0200

    attribute für User festgelegt

--- src/main/java/de/thaizen/webforum/model/User.java
+++ src/main/java/de/thaizen/webforum/model/User.java
@@ -1,4 +1,15 @@
 package de.thaizen.webforum.model;
 
 public class User {
+    private String username;
+    private String password;
+    private String email;
+    private String role;
+    private String token;
+
+
+    public String getUsername() {
+        return username;
+    }
 }
+
