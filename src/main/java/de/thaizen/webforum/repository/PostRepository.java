454fc52405c0b8832b7d7b8baa878ac372852325
ThaiZen040 <thanagorn.jaroenkal@gmail.com>
2026-06-24 00:15:33 +0200

    Created PostRepository.java Created TopicRepository.java

---
+++ src/main/java/de/thaizen/webforum/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package de.thaizen.webforum.repository;
+
+import de.thaizen.webforum.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
