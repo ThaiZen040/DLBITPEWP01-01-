a7bb70500df3cd14c8e5e7b7e8e65520e19bdf24
ThaiZen040 <thanagorn.jaroenkal@gmail.com>
2026-06-24 01:03:03 +0200

    CRUD added to UserService.java

--- src/main/java/de/thaizen/webforum/service/UserService.java
+++ src/main/java/de/thaizen/webforum/service/UserService.java
@@ -1,7 +1,11 @@
 package de.thaizen.webforum.service;
 
+import de.thaizen.webforum.model.User;
 import de.thaizen.webforum.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -10,5 +14,21 @@ public class UserService {
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
     }
 }
