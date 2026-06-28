68fb29c370f02e10e354eb760b751795320619e5
ThaiZen040 <thanagorn.jaroenkal@gmail.com>
2026-06-28 05:00:17 +0200

    PostService added createPost

--- src/main/java/de/thaizen/webforum/service/PostService.java
+++ src/main/java/de/thaizen/webforum/service/PostService.java
@@ -1,4 +1,21 @@
 package de.thaizen.webforum.service;
 
+import de.thaizen.webforum.model.Post;
+import de.thaizen.webforum.repository.PostRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PostService {
+    // Zugriff auf die Datenbank
+    private final PostRepository postRepository;
+
+    //Konstruktor
+    public  PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+    public Post createPost(Post post)
+    {
+        return postRepository.save(post);
+    }
+
 }
