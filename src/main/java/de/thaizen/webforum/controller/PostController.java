ec5742448186822e5702d9370a5d6a3e1694819b
ThaiZen040 <thanagorn.jaroenkal@gmail.com>
2026-06-29 21:34:00 +0200

    added PostController

---
+++ src/main/java/de/thaizen/webforum/controller/PostController.java
@@ -0,0 +1,17 @@
+package de.thaizen.webforum.controller;
+
+import de.thaizen.webforum.service.PostService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+
+    private final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+}
