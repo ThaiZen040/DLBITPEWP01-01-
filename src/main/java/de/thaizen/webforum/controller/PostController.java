56a03724f7bb9c9750c2656d23153dc19f3864a1
ThaiZen040 <154074568+ThaiZen040@users.noreply.github.com>
2026-07-01 22:07:39 +0200

    added PostController with CRUD operations

--- src/main/java/de/thaizen/webforum/controller/PostController.java
+++ src/main/java/de/thaizen/webforum/controller/PostController.java
@@ -2,16 +2,14 @@ package de.thaizen.webforum.controller;
 
 import de.thaizen.webforum.model.Post;
 import de.thaizen.webforum.service.PostService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/posts")
 public class PostController {
 
-    //Zugriff auf die Geschäftslogik
     private final PostService postService;
 
     // Dependency Injection
@@ -19,10 +17,34 @@ public class PostController {
         this.postService = postService;
     }
 
-    //REST API
+    // POST: Beitrag erstellen
     @PostMapping
     public Post createPost(@RequestBody Post post) {
         return postService.createPost(post);
     }
 
+    // GET: Alle Beiträge anzeigen
+    @GetMapping
+    public List<Post> getAllPosts() {
+        return postService.findAllPosts();
+    }
+
+    // GET: Einzelnen Beitrag nach ID anzeigen
+    @GetMapping("/{id}")
+    public Post getPostById(@PathVariable Long id) {
+        return postService.findPostById(id);
+    }
+
+    // PUT: Beitrag bearbeiten
+    @PutMapping("/{id}")
+    public Post updatePost(@PathVariable Long id, @RequestBody Post post) {
+        post.setId(id);
+        return postService.updatePost(post);
+    }
+
+    // DELETE: Beitrag löschen
+    @DeleteMapping("/{id}")
+    public void deletePost(@PathVariable Long id) {
+        postService.deletePost(id);
+    }
 }
