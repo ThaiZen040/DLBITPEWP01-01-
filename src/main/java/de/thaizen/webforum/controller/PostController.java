e9f3d0933180859b16a3d40bad27c7f4f22fdd60
ThaiZen040 <154074568+ThaiZen040@users.noreply.github.com>
2026-07-01 16:04:47 +0200

    PostMapping createPost added

--- src/main/java/de/thaizen/webforum/controller/PostController.java
+++ src/main/java/de/thaizen/webforum/controller/PostController.java
@@ -1,7 +1,10 @@
 package de.thaizen.webforum.controller;
 
+import de.thaizen.webforum.model.Post;
 import de.thaizen.webforum.service.PostService;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,6 +17,12 @@ public class PostController {
     // Dependency Injection
     public PostController(PostService postService) {
         this.postService = postService;
+    }
+
+    //REST API
+    @PostMapping
+    public Post createPost(@RequestBody Post post) {
+        return postService.createPost(post);
     }
 
 }
