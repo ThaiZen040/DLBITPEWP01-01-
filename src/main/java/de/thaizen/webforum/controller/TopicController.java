77f7087f95f0fa5abf23653006c06b666dee5f8b
ThaiZen040 <154074568+ThaiZen040@users.noreply.github.com>
2026-07-01 15:12:59 +0200

    TopicController REST-API implemented

--- src/main/java/de/thaizen/webforum/controller/TopicController.java
+++ src/main/java/de/thaizen/webforum/controller/TopicController.java
@@ -1,4 +1,41 @@
 package de.thaizen.webforum.controller;
 
+import de.thaizen.webforum.model.Topic;
+import de.thaizen.webforum.service.TopicService;
+import org.springframework.web.bind.annotation.*;
+
+//
+@RestController
+@RequestMapping("/topics")
 public class TopicController {
+
+    // Zugriff auf den Service
+    private final TopicService topicService;
+
+    // Dependency Injection
+    public TopicController(TopicService topicService) {
+        this.topicService = topicService;
+    }
+
+    // Ruft den Service auf, um alle Themen abzurufen
+    @GetMapping
+    public Iterable<Topic> getAllTopics() {
+        return topicService.getAllTopics();
+    }
+    // Ruft den Service auf, um ein neues Thema zu erstellen
+    @PostMapping
+    public Topic createTopic(@RequestBody Topic topic) { // Spring benoetigt @RequestBody, um den Inhalt der Anfrage zu lesen
+        return topicService.createTopic(topic);
+    }
+
+    // Ruft den Service auf, um ein Thema zu aktualisieren
+    @PutMapping("/{id}")
+    public Topic updateTopic(@PathVariable Long id, @RequestBody Topic topic) {
+        return topicService.updateTopic(topic);
+    }
+    // Löscht ein Thema anhand der ID
+    @DeleteMapping("/{id}")
+    public void deleteTopic(@PathVariable Long id) {
+        topicService.deleteTopic(id);
+    }
 }
