7abc4b1d9aa0d57d74d96de7033104655de26456
ThaiZen040 <154074568+ThaiZen040@users.noreply.github.com>
2026-07-01 16:07:25 +0200

    Kommentare hinzugefügt

--- src/main/java/de/thaizen/webforum/controller/UserController.java
+++ src/main/java/de/thaizen/webforum/controller/UserController.java
@@ -7,22 +7,23 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class UserController {
 
+    // Zugriff auf den Service
     private final UserService userService;
-
+    // Dependency Injection
     public UserController(UserService userService) {
         this.userService = userService;
     }
-
+    // REST API - Registrierung
     @PostMapping("/register")
     public User registerUser(@RequestBody User user) {
         return userService.createUser(user);
     }
-
+    // Rest API - Login
     @PutMapping("/users/{id}")
     public User updateUser(@PathVariable Long id, @RequestBody User user) {
         return userService.updateUser(user);
     }
-
+    // Delete User
     @DeleteMapping("/users/{id}")
     public void deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
